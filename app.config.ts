777963c78703563964f1ffdad06ede6f94aacc16
Bora ALAP <bora.alap@artticfox.com>
2024-06-02 14:51:52 +0200

    chore: Update app version to 1.1.0

--- app.config.ts
+++ app.config.ts
@@ -34,7 +34,7 @@ module.exports = ({ config }: ConfigContext): ExpoConfig => ({
   name: appName,
   slug: "allergii",
   owner: "artticfox",
-  version: "1.0.0",
+  version: "1.1.0",
   orientation: "portrait",
   icon: "./assets/appicon.png",
   scheme: "myapp",
